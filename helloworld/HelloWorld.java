ec0bf423f107ec70186f9b54d60fe999b1a2158c
AnniePiotrowski <anniekatherine.piotrowski@gmail.com>
2024-05-05 20:17:35 -0700

    Add HelloWorld java example + a .gitignore file (to avoid accidentally commiting .iml or .class files.

---
+++ helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    /**
+     * How to run this program?
+     * From the command prompt, navigate to the learnjava/helloworld directory.
+     * Run javac HelloWorld.java to compile it (turn it into code that is machine-readable.)
+     * Then run java HelloWorld to see the results of the program printed in your command prompt.
+     * Want to see a COMPILE-TIME error? Make a mistake in this program - remove a parenthesis or semicolon,
+     * or misspell System.out.println and try to recompiling. You'll get an error!
+     */
+    public static void main(String... args) {
+        System.out.println("Hello, World!");
+    }
+}
